cd8ef03defdd7f804109a8587968e72832e8df46
saim khalid <132395912+SaimKhalid1@users.noreply.github.com>
2023-11-29 16:37:29 -0700

    Update Airline.h

--- FinalProject/Airline.h
+++ FinalProject/Airline.h
@@ -1,11 +1,21 @@
+// Airline.h
 #ifndef OUR_AIRLINE
 #define OUR_AIRLINE
 
-class Airline {
-    public:
+#include <vector>
+#include <string>
+#include "Flight.h"
 
-    private:
-        
+class Our_Airline {
+public:
+    Our_Airline();
+
+    void addFlight(const Our_Flight& flight);
+    Our_Flight getFlight(const std::string& flight_id) const;
+
+private:
+    std::vector<Our_Flight> flights;
 };
 
 #endif
+
