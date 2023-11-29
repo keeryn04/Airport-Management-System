b5175feeba84f23dd4e94eb35808a2d841b6b868
saim khalid <132395912+SaimKhalid1@users.noreply.github.com>
2023-11-29 16:37:52 -0700

    Update Airline.cpp

--- FinalProject/Airline.cpp
+++ FinalProject/Airline.cpp
@@ -1,8 +1,21 @@
-#include <iostream>
-using namespace std;
-
+// Airline.cpp
 #include "Airline.h"
 
-int main(void) {
-
+Our_Airline::Our_Airline() {
+    // Constructor implementation if needed
 }
+
+void Our_Airline::addFlight(const Our_Flight& flight) {
+    flights.push_back(flight);
+}
+
+Our_Flight Our_Airline::getFlight(const std::string& flight_id) const {
+    for (const auto& flight : flights) {
+        if (flight.get_flight_id() == flight_id) {
+            return flight;
+        }
+    }
+    // If flight_id is not found, return an empty flight
+    return Our_Flight();
+}
+
